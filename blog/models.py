132f629f8592262d94723b079856022b58000f1c
Nokjao <Macbook@KnickKnack.local>
2014-04-05 10:10:44 +0700

    Add a template filter to archive.html

--- blog/models.py
+++ blog/models.py
@@ -2,13 +2,18 @@ from django.db import models
 from django.contrib import admin
 
 class BlogPost(models.Model):
-    """Model for posting on blog"""
+    """Database model for posting on blog"""
     title = models.CharField(max_length=150)
     body = models.TextField()
     timestamp = models.DateTimeField()
 
     def __unicode__(self):
+        """Prettify the name of the instance when queried upon"""
         return self.title
-
+    
+    class Meta:
+        """Reverse-chronologically ordered post.
+           Omit the '-' to do it in ascending order"""
+        ordering = ('-timestamp',)
 
 
